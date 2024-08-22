ed5790e2bb9a06a6e7ce588a31a7b6734562c03d
JUNGHOON AN <gnstm831@naver.com>
2024-08-22 20:48:17 +0900

    섹션2-4. 클래스의 분리

--- hellospring/src/main/java/tobyspring/hellospring/PaymentService.java
+++ hellospring/src/main/java/tobyspring/hellospring/PaymentService.java
@@ -4,14 +4,18 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-abstract public class PaymentService {
+public class PaymentService {
+    private final SimpleExRateProvider exRateProvider;
+
+    public PaymentService() {
+        this.exRateProvider = new SimpleExRateProvider();
+    }
+
     public Payment prepare(Long orderId, String currency, BigDecimal foreginCurrencyAmount) throws IOException {
-        BigDecimal exRate = getExRate(currency);
+        BigDecimal exRate = exRateProvider.getExRate(currency);
         BigDecimal convertedAmount = foreginCurrencyAmount.multiply(exRate);
         LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
 
         return new Payment(orderId, currency, foreginCurrencyAmount, exRate, convertedAmount, validUntil);
     }
-
-    abstract BigDecimal getExRate(String currency) throws IOException;
 }

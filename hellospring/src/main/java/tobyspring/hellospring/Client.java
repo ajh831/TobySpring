6f9a3f51de46d4277d96d1809298c048e33c652d
JUNGHOON AN <gnstm831@naver.com>
2024-08-23 17:17:09 +0900

    섹션3-7. 오브젝트 팩토리

--- hellospring/src/main/java/tobyspring/hellospring/Client.java
+++ hellospring/src/main/java/tobyspring/hellospring/Client.java
@@ -5,8 +5,9 @@ import java.math.BigDecimal;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-//        PaymentService paymentService = new PaymentService(new WebApiExRateProvider()); // 관계설정의 책임을 Client한테 넘김
-        PaymentService paymentService = new PaymentService(new SimpleExRateProvider());
+        ObjectFactory objectFactory = new ObjectFactory();
+        PaymentService paymentService = objectFactory.paymentService();
+
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
     }

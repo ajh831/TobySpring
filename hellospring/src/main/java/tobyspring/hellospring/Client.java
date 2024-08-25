10a96108a3f3879f199a23e6e7b7e0df902530a6
JUNGHOON AN <gnstm831@naver.com>
2024-08-25 15:12:55 +0900

    섹션3-13. DI와 디자인 패턴

--- hellospring/src/main/java/tobyspring/hellospring/Client.java
+++ hellospring/src/main/java/tobyspring/hellospring/Client.java
@@ -5,18 +5,27 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.concurrent.TimeUnit;
 
 public class Client {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         BeanFactory benFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
         PaymentService paymentService = benFactory.getBean(PaymentService.class);
-        PaymentService paymentService2 = benFactory.getBean(PaymentService.class);
 
-        System.out.println("paymentService = " + paymentService);
-        System.out.println("paymentService2 = " + paymentService2);
-        System.out.println("paymentService == paymentService2 = " + (paymentService == paymentService2));
+        Payment payment1 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
+        System.out.println("Payment1: " + payment1);
 
-        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
-        System.out.println(payment);
+        System.out.println("------------------------------------\n");
+
+        TimeUnit.SECONDS.sleep(1); // 1초 대기
+
+        Payment payment2 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
+        System.out.println("Payment2: " + payment2);
+        System.out.println("------------------------------------\n");
+
+        TimeUnit.SECONDS.sleep(2); // 2초 대기
+
+        Payment payment3 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
+        System.out.println("Payment3: " + payment3);
     }
 }

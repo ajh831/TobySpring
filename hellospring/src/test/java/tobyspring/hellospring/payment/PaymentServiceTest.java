226669b9bd9da920ee6e48cfa3d45cd6368fefb5
JUNGHOON AN <gnstm831@naver.com>
2024-08-25 23:32:08 +0900

    섹션4-5. 테스트와 DI(1)

--- hellospring/src/test/java/tobyspring/hellospring/payment/PaymentServiceTest.java
+++ hellospring/src/test/java/tobyspring/hellospring/payment/PaymentServiceTest.java
@@ -1,6 +1,7 @@
 package tobyspring.hellospring.payment;
 
 import org.assertj.core.api.Assertions;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import tobyspring.hellospring.exrate.WebApiExRateProvider;
@@ -9,8 +10,8 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import static java.math.BigDecimal.valueOf;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class PaymentServiceTest {
 
@@ -32,5 +33,28 @@ class PaymentServiceTest {
         assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
         assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMonths(30));
 
+    }
+
+    @Test
+    @DisplayName("금액과 관련하여 convertedAmount 계산을 정확하게 하는지 검증")
+    void convertedAmount() throws IOException {
+        testAmount(valueOf(500), valueOf(5_000));
+        testAmount(valueOf(1_000), valueOf(10_000));
+        testAmount(valueOf(3_000), valueOf(30_000));
+
+        // 원화환산금액의 유효시간 계산
+//        assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
+//        assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMonths(30));
+
+    }
+
+    @NotNull
+    private static void testAmount(BigDecimal exRate, BigDecimal convertedAmount) throws IOException {
+        PaymentService paymentService = new PaymentService(new ExRateProviderStub(exRate));
+
+        Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+
+        assertThat(payment.getExRate()).isEqualByComparingTo(exRate);
+        assertThat(payment.getConvertedAmount()).isEqualTo(convertedAmount);
     }
 }

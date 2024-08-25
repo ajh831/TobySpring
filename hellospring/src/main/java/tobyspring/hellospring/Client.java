97cf4f0c82f0dc2b19f8dd9e4885360c85e21f69
JUNGHOON AN <gnstm831@naver.com>
2024-08-25 19:20:52 +0900

    섹션4-1. 자동으로 수행되는 테스트

--- hellospring/src/main/java/tobyspring/hellospring/Client.java
+++ hellospring/src/main/java/tobyspring/hellospring/Client.java
@@ -7,27 +7,13 @@ import tobyspring.hellospring.payment.PaymentService;
 
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.util.concurrent.TimeUnit;
 
 public class Client {
     public static void main(String[] args) throws IOException, InterruptedException {
         BeanFactory benFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
         PaymentService paymentService = benFactory.getBean(PaymentService.class);
 
-        Payment payment1 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
-        System.out.println("Payment1: " + payment1);
-
-        System.out.println("------------------------------------\n");
-
-        TimeUnit.SECONDS.sleep(1); // 1초 대기
-
-        Payment payment2 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
-        System.out.println("Payment2: " + payment2);
-        System.out.println("------------------------------------\n");
-
-        TimeUnit.SECONDS.sleep(2); // 2초 대기
-
-        Payment payment3 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
-        System.out.println("Payment3: " + payment3);
+        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
+        System.out.println("Payment: " + payment);
     }
 }
--- hellospring/src/main/java/tobyspring/hellospring/ObjectFactory.java
+++ hellospring/src/main/java/tobyspring/hellospring/ObjectFactory.java
@@ -2,21 +2,15 @@ package tobyspring.hellospring;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import tobyspring.hellospring.exrate.CachedExRateProvider;
-import tobyspring.hellospring.payment.ExRateProvider;
 import tobyspring.hellospring.exrate.WebApiExRateProvider;
+import tobyspring.hellospring.payment.ExRateProvider;
 import tobyspring.hellospring.payment.PaymentService;
 
 @Configuration
 public class ObjectFactory {
     @Bean
     public PaymentService paymentService() {
-        return new PaymentService(cachedExRateProvider());
-    }
-
-    @Bean
-    public ExRateProvider cachedExRateProvider() {
-        return new CachedExRateProvider(exRateProvider());
+        return new PaymentService(exRateProvider());
     }
 
     @Bean

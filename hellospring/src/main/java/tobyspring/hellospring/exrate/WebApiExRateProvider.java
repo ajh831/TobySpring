c083e082f638670a259083dcbe9c66756d4805d0
JUNGHOON AN <gnstm831@naver.com>
2024-08-26 18:40:02 +0900

    섹션5-9. ApiTemplate 분리

--- hellospring/src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
+++ hellospring/src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
@@ -1,52 +1,21 @@
 package tobyspring.hellospring.exrate;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import tobyspring.hellospring.api.ApiExcutor;
+import tobyspring.hellospring.api.ApiTemplate;
 import tobyspring.hellospring.api.ErApiExRateExtractor;
-import tobyspring.hellospring.api.ExRateExtractor;
-import tobyspring.hellospring.api.SimpleApiExecutor;
+import tobyspring.hellospring.api.HttpClientApiExecutor;
 import tobyspring.hellospring.payment.ExRateProvider;
 
-import java.io.IOException;
 import java.math.BigDecimal;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 //@Component
 public class WebApiExRateProvider implements ExRateProvider {
+    ApiTemplate apiTemplate = new ApiTemplate();
+
     @Override
     public BigDecimal getExRate(String currency) {
+
         String url = "https://open.er-api.com/v6/latest/" + currency;
-/*
-        람다식으로 콜백을 만들어서 던지면 굳이 클래스를 만들지 않아도 됨
-        return runApiForExRate(url, new SimpleApiExecutor(), response -> {
-            ObjectMapper mapper = new ObjectMapper();
-            ExRateData data = mapper.readValue(response, ExRateData.class);
-            return data.rates().get("KRW");
-        });
-*/
-        return runApiForExRate(url, new SimpleApiExecutor(), new ErApiExRateExtractor());
-    }
 
-    private static BigDecimal runApiForExRate(String url, ApiExcutor apiExcutor, ExRateExtractor exRateExtractor) {
-        URI uri;
-        try {
-            uri = new URI(url);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-
-        String response;
-        try {
-            response = apiExcutor.execute(uri);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        try {
-            return exRateExtractor.extract(response);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+        return apiTemplate.getExRate(url, new HttpClientApiExecutor(), new ErApiExRateExtractor());
     }
 }
